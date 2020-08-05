ae5460e704a0ee17cdab7005c9f57f79b50222b5
Phamtung1802 <phamtung.0218@gmail.com>
2020-08-05 15:46:34 +0700

    bb

--- MainProject/src/sample/Main.java
+++ MainProject/src/sample/Main.java
@@ -9,7 +9,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.util.HashMap;
 
 public class Main extends Application {
@@ -26,13 +25,12 @@ public class Main extends Application {
     public static void main(String[] args) {
 //        launch(args);
 //        list.put("1",new Book("Cuon Sach 1",1000, "01/06/2007","Tac Gia 1","NXB Kim Dong","Giao Duc","001"));
-
 //        System.out.println(DataManager.getINSTANCE().addBook("001","English","Cuon Sach 1",1000, "01/06/2007","Tac Gia 1","NXB Kim Dong","Giao Duc"));
 //        System.out.println(DataManager.getINSTANCE().addBook("002","English","Cuon Sach 2",1000, "01/06/2007","Tac Gia 1","NXB Kim Dong","Giao Duc"));
 //        DataManager.getINSTANCE().appendData("appendtest.txt");
         DataManager.getINSTANCE().read();
         System.out.println(DataManager.getINSTANCE().getLib());
-        System.out.println(DataManager.getINSTANCE().addBook("1","English","Cuon Sach 2",1000, "01/06/2007","Tac Gia 1","NXB Kim Dong","Giao Duc"));
+        System.out.println(DataManager.getINSTANCE().addBook("5","English","Cuon Sach 2",1000, "01/06/2007","Tac Gia 1","NXB Kim Dong","Giao Duc"));
         DataManager.getINSTANCE().WriteFile();
 
     }
